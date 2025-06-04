2232b7fd019c902cef54223ba03316e667f42617
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-06-04 21:11:10 +0800

    Add new database

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 let zodiacData = [];
 fetch("data.json")
-    .then(res => res.json())
-    .then(data => {zodiacData = data})
-    .catch(err => console.error("Cannot fetch data.json: " + err))
+	.then(res => res.json())
+	.then(data => { zodiacData = data })
+	.catch(err => console.error("Cannot fetch data.json: " + err))
+
+const isDateBetweenDateStrings = (date, startDateString, endDateString) => {
+	let startDate = new Date(startDateString);
+	let endDate = new Date(endDateString);
+	return date.valueOf() >= startDate.valueOf() && date.valueOf() <= endDate.valueOf()
+}
+
 
 document.getElementById("zodiacForm").addEventListener("submit", (e) => {
-  e.preventDefault();
-  const birthDate = new Date(document.getElementById("birthdate").value);
-  const year = birthDate.getFullYear();
+	e.preventDefault();
+	const birthDate = new Date(document.getElementById("birthdate").value);
+	const year = birthDate.getFullYear();
 
-  if (isNaN(year)) return;
-  let yearData = zodiacData.find(data => data["Year"] == year);
-  document.getElementById("animal").textContent = yearData["Zodiac"];
-  document.getElementById("description").textContent = yearData["Personality Traits"];
-  document.getElementById("element").textContent = yearData["Element"]
-  document.getElementById("result").classList.remove("hidden");
+	if (isNaN(year)) return;
+	let yearData = zodiacData.find(data => isDateBetweenDateStrings(birthDate, data["Start Date"], data["End Date"]));
+	document.getElementById("animal").textContent = yearData["Zodiac"];
+	document.getElementById("element").textContent = yearData["Element"]
+	document.getElementById("qualities").textContent = yearData["Qualities"];
+	document.getElementById("weaknesses").textContent = yearData["Weaknesses"];
+	document.getElementById("most-compatible").textContent = yearData["Most Compatible"];
+	document.getElementById("least-compatible").textContent = yearData["Least Compatible"];
+	document.getElementById("result").classList.remove("hidden");
 });
